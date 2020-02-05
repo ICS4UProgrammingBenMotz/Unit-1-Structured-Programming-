1f440451f856960f641a783b7d4cdf7136b5fff4
benmotz2002 <43444941+benmotz2002@users.noreply.github.com>
2020-02-05 12:03:14 -0500

    Initial commit

---
+++ HelloWorld/src/HelloWorld.java
@@ -0,0 +1,11 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Prints "Hello World in console
+		System.out.println("Hello world!");
+
+	}
+
+}
