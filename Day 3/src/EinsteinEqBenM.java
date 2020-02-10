c3ee5e12233f0380116ccdbdd00fdf03c9bfea2e
benmotz2002 <43444941+benmotz2002@users.noreply.github.com>
2020-02-10 13:02:28 -0500

    simpcalc

--- Day 3/src/EinsteinEqBenM.java
+++ Day 3/src/EinsteinEqBenM.java
@@ -5,17 +5,22 @@ public class EinsteinEqBenM {
 	public static void main(String[] args) {
 		final double SPEED_OF_LIGHT=2.998*Math.pow(10, 8);
 		
+		//creates variables
 		double mass;
 		double energy;
 		
+		//prints out question for the user
 		System.out.print("Welcome to Einstein's Calculator\nPlease enter your mass");
 		
+		//accepts the users entry
 		Scanner EntMass=new Scanner(System.in);
 		
 		mass = EntMass.nextDouble();
 		
+		//calculates the energy
 		energy = mass * Math.pow(SPEED_OF_LIGHT, 2);
 		
+		//displays the energy
 		System.out.print("The energy is: " + energy + " Joules" );
 		
 		
