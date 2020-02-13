f05a4ce7681b38c14398af0a7b35dc16c36da6c9
benmotz2002 <43444941+benmotz2002@users.noreply.github.com>
2020-02-13 12:08:56 -0500

    o0k

---
+++ src/DiceGame.java
@@ -0,0 +1,8 @@
+import javax.swing.*;
+
+public class DiceGame {
+    private JLabel lblGuess;
+    private JLabel lblNumbOGuess;
+    private JLabel lblCorrectNumb;
+    private JComboBox cmbNumbers;
+}
