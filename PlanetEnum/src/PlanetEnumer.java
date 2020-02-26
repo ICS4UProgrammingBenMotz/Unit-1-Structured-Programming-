e2b296a86c42623ce3c584c5d7e966940c0009c0
benmotz2002 <43444941+benmotz2002@users.noreply.github.com>
2020-02-26 12:39:13 -0500

    sadfasd

---
+++ PlanetEnum/src/PlanetEnumer.java
@@ -0,0 +1,66 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+
+
+public class PlanetEnumer {
+    private JComboBox cmbPlanets;
+    private JButton btnSelectPlanet;
+    private javax.swing.JPanel JPanel;
+
+
+    //creates enum
+    public enum Planets{
+    Mercury, Venus, Earth, Mars, Jupiter, Saturn, Uranus, Neptune
+    }
+
+    public PlanetEnumer() {
+        //adds values to cmb
+        for (Planets p:Planets.values()){
+            cmbPlanets.addItem(p);
+        }
+
+        btnSelectPlanet.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+
+                //checks what the user selected, will then display a window for a response to the selection
+                if (cmbPlanets.getSelectedItem()==Planets.Mercury){
+                    JOptionPane.showMessageDialog(null, "You want to go to " +Planets.Mercury + ", planet #1");
+                }
+                else if (cmbPlanets.getSelectedItem()==Planets.Venus){
+                    JOptionPane.showMessageDialog(null, "You want to go to " +Planets.Venus + ", planet #2");
+                }
+                else if (cmbPlanets.getSelectedItem()==Planets.Earth){
+                    JOptionPane.showMessageDialog(null, "You want to go to " +Planets.Earth + ", planet #3");
+                }
+                else if (cmbPlanets.getSelectedItem()==Planets.Mars){
+                    JOptionPane.showMessageDialog(null, "You want to go to " +Planets.Mars + ", planet #4");
+                }
+                else if (cmbPlanets.getSelectedItem()==Planets.Jupiter){
+                    JOptionPane.showMessageDialog(null, "You want to go to " +Planets.Jupiter + ", planet #5");
+                }
+                else if (cmbPlanets.getSelectedItem()==Planets.Saturn){
+                    JOptionPane.showMessageDialog(null, "You want to go to " +Planets.Saturn + ", planet #6");
+                }
+                else if (cmbPlanets.getSelectedItem()==Planets.Uranus){
+                    JOptionPane.showMessageDialog(null, "You want to go to " +Planets.Uranus + ", planet #7");
+                }
+                else if (cmbPlanets.getSelectedItem()==Planets.Neptune){
+                    JOptionPane.showMessageDialog(null, "You want to go to " +Planets.Neptune + ", planet #8");
+                }
+
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("App");
+        frame.setContentPane(new PlanetEnumer().JPanel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+}
