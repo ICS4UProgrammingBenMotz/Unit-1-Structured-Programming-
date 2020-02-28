e7bbb74eae6f6656b1b6bb7d950a939c347a682c
benmotz2002 <43444941+benmotz2002@users.noreply.github.com>
2020-02-28 12:08:15 -0500

    Update HelloWorld.java

--- Day 1/HelloWorld/src/HelloWorld.java
+++ Day 1/HelloWorld/src/HelloWorld.java
@@ -1,3 +1,10 @@
+/*
+ * Created by: Ben Motz
+ * Created on: 28-02-2020
+ * Created for: ICS4U Programming
+ * Daily Assignment – Day 2 - HelloWorld
+ * This program displays hello world
+*/
 
 public class HelloWorld {
 
